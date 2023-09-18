a1da697550ad773df4515a5c787baf94240487d0
prabhjeet9433 <50940003+prabhjeet9433@users.noreply.github.com>
2023-09-18 13:03:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Currency from "./currency/currency";
+
+function App() {
+  return (
+    <div className="App">
+      <Currency />
+    </div>
+  );
+}
+
+export default App;
